15c050f2067990a1b6d9b8bd8a0b1c6b1a8c7576
eyeya <eyephantana2001@hotmail.com>
2024-07-08 16:30:08 +0700

    newpage elt

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
+// const port = import.meta.env.VITE_PORT
 export default defineConfig({
   plugins: [react()],
   server: {
     strictPort: true,
     host: true,
-    port: 80, //80
+    port: 1234, //80
   },
 });
