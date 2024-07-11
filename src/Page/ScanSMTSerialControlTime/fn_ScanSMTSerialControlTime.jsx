a40a57809cd8ee62b058f5aab107b9179e10361b
milk953 <milk.the@hotmail.com>
2024-07-11 16:29:32 +0700

    addnewpage

---
+++ src/Page/ScanSMTSerialControlTime/fn_ScanSMTSerialControlTime.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+
+function fn_ScanSMTSerialControlTime() {
+    
+
+    return {
+        
+    }
+};
+
+export { fn_ScanSMTSerialControlTime };
