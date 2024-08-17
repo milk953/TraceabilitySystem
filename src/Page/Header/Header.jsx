fdd26e8e1e1897d52ddfcbd1f13c419fc5adf1ad
Mayxsasii <Lovemay19123@gmail.com>
2024-08-17 13:33:44 +0700

    new page

--- src/Page/Header/Header.jsx
+++ src/Page/Header/Header.jsx
@@ -89,7 +89,8 @@ function Hearder() {
             </IconButton>
             <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar} />
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              TRACEABILITY SYSTEM  {menuName}
+              {/* TRACEABILITY SYSTEM  {menuName} */}
+              TRACEABILITY SYSTEM
             </Typography>
 
             {user ? <> <Avatar
