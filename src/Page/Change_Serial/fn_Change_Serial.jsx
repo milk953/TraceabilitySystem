fba4d6321e0f8101b1c6333204fade2c23c2bd34
khunpan9551 <paiboon.wongthongdee@gmail.com>
2024-09-11 09:15:43 +0700

    commit by khun 11/09/2024

---
+++ src/Page/Change_Serial/fn_Change_Serial.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { Tag } from "antd";
+function fn_Change_Serial() {
+  
+  //PageLoad----------
+  useEffect(() => {
+  
+  }, []);
+  //-------------------
+  const columns = [
+    {
+      title: "No.",
+      dataIndex: "SEQ",
+      key: "No.",
+      render: (text, record, index) => {
+        return index + 1;
+      },
+      align: "center",
+    },
+    {
+      title: "Old Partial No.",
+      dataIndex: "SERIAL_OLD",
+      key: "Old Partial No.",
+      align: "left",
+      render: (text, record, index) => {
+          return text;
+      },
+    },
+    {
+      title: "New Partial No.",
+      dataIndex: "SERIAL_NEW",
+      key: "New Partial No.",
+      align: "center",
+      render: (text, record, index) => {
+        return text;
+      },
+    },
+
+    {
+      title: "Count",
+      key: "Count",
+      dataIndex: "FINALGATE_ROW",
+      align: "center",
+      render: (text, record, index) => {
+          return text;
+      },
+    },
+    
+  ];
+
+  return {
+  
+    columns
+  };
+}
+
+export { fn_Change_Serial };
