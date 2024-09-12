d7f49d4a67f6270a374bde7bcfdbd1b91f6d2a72
khunpan9551 <paiboon.wongthongdee@gmail.com>
2024-09-12 09:19:27 +0700

    commit by khun 12/09/2024

--- src/Page/Change_Serial/Change_Serial.jsx
+++ src/Page/Change_Serial/Change_Serial.jsx
@@ -33,35 +33,38 @@ import Hearder from "../Header/Header";
 import BackspaceIcon from "@mui/icons-material/Backspace";
 import "../Common/StyleCommon.css";
 import { fn_Change_Serial } from "../Change_Serial/fn_Change_Serial";
+
 function Change_Serial() {
-  const { columns } = fn_Change_Serial();
+  const { txtTotalPcs,txtTotalPcs_TextChanged,BtnSubmit_Click } = fn_Change_Serial();
 
   return (
     <div>
       <Hearder />
-      <h1>Serial Config.</h1>
+      <h1>Replace Partial No.</h1>
       <Card component={Paper} className="Card-Common">
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-       
-            <Grid item xs={10} md={5}>
-              <Table component={Card} className="ChangeSerial">
-                {/* <TableHead style={{ height: "60px" }}>
-            <TableRow>
-              <TableCell
-                colSpan={3}
-                align="center"
-                style={{ fontSize: "30px" }}
+        <Box sx={{ display: "flex", alignItems: "flex-start" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={10} md={12} align="center">
+              {/* <Paper
+                className="Card-lblResult"
+                style={{
+                  background: "#059212",
+                  marginBottom: "10px",
+                  width: "40%",
+                }}
               >
-                <b>Serial Config.</b>
-              </TableCell>
-            </TableRow>
-          </TableHead> */}
+                <Typography
+                  variant="h4"
+                  style={{ paddingTop: "5px", color: "#fff" }}
+                >
+      
+                </Typography>
+              </Paper> */}
+              <Table
+                component={Card}
+                className="ChangePartino"
+                style={{ width: "50%" }}
+              >
                 <TableBody>
                   <TableRow>
                     <TableCell align="right" style={{ width: "150px" }}>
@@ -73,6 +76,7 @@ function Change_Serial() {
                         className="input_txt"
                         style={{ width: "50%" }}
                         fullWidth
+                        value={txtTotalPcs.value}
                       />
                     </TableCell>
                   </TableRow>
@@ -81,6 +85,7 @@ function Change_Serial() {
               <Table
                 className="CSS-GvSerial"
                 style={{
+                  width: "50%",
                   marginTop: "20px",
                 }}
                 component={Card}
@@ -102,68 +107,19 @@ function Change_Serial() {
                   <TableRow></TableRow>
                 </TableHead>
                 <TableBody>
-                  {/* <TableRow> */}
-
-                  {/* {Array.from({ length: gvSerial.value.length }, (_, index) => (
-                    <TableRow key={index}>
-                      <TableCell
-                        align="center"
-                        sx={{ borderRight: "1px solid #d9d9d9" }}
-                      >
-                        {index + 1}
-                      </TableCell>
-                      <TableCell>
-                        <TextField
-                          key={index}
-                          className="input_txt"
-                          size="small"
-                          fullWidth
-                          inputRef={(el) => (fc_txtSerial.current[index] = el)}
-                          value={txtSerial[index]}
-                          onBlur={(event) => {
-                            handleSerialChange(index, event);
-                          }}
-                          onChange={(event) => handleSerialChange(index, event)}
-                          onKeyDown={(event) => {
-                            if (event.key === "Enter") {
-                              event.preventDefault(); 
-                              if (index < gvSerial.value.length - 1) {
-                                fc_txtSerial.current[index + 1].focus();
-                              } else {
-                                btnSave_Click();
-                                event.target.blur();
-                              }
-                            }
-                          }}
-                        />
-                      </TableCell>
-                    </TableRow>
-                  ))} */}
-
                   <TableRow>
-                    <TableCell colSpan={2} style={{ textAlign: "center" }}>
-                      <Button
-                        className="BtSave"
-                        // onClick={btnSave_Click}
-                      >
-                        Save
-                      </Button>{" "}
-                      &nbsp;&nbsp;
-                      <Button
-                        className="BtCancel"
-                        // onClick={btnCancel_Click}
-                      >
-                        Cancel
+                    <TableCell colSpan={3} style={{ textAlign: "center" }}>
+                      <Button className="BtSave" onClick={BtnSubmit_Click} >
+                        Submit
                       </Button>
                     </TableCell>
                   </TableRow>
                 </TableBody>
               </Table>
             </Grid>
-     
+          </Grid>
         </Box>
       </Card>
-      {/* </Card> */}
     </div>
   );
 }
