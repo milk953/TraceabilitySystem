344d6fa404c6b3b8da7e5a1e32e30e28860b2b25
Mayxsasii <Lovemay19123@gmail.com>
2024-09-20 18:08:02 +0700

    final gate only good suecces

--- src/Page/Confirm Final Only Good/ScanSMTSerialPcsAutoTrayConfirm.jsx
+++ src/Page/Confirm Final Only Good/ScanSMTSerialPcsAutoTrayConfirm.jsx
@@ -31,7 +31,7 @@ import "./SerialPcs.css";
 import { Table as AntTable } from 'antd';
 import "../Common/StyleCommon.css";
 import Hearder from "../Header/Header";
-import { fn_ScanSMTSerialPcsChrome } from "./fn_ScanSMTSerialPcsAutoTrayConfirm";
+import { fn_ScanSMTSerialPcsAutoTrayConfirm } from "./fn_ScanSMTSerialPcsAutoTrayConfirm";
 import Pageimg from "/src/assets/1.jpg";
 function ScanSMTRoollSht() {
   const {
@@ -68,8 +68,8 @@ function ScanSMTRoollSht() {
     lblResult,
     lblTime,
     columns
-  } = fn_ScanSMTSerialPcsChrome();
-  console.log("lblTime", lblTime);
+  } = fn_ScanSMTSerialPcsAutoTrayConfirm();
+
   return (
     // <div>
       <>
@@ -315,7 +315,7 @@ function ScanSMTRoollSht() {
                       className="Card-lblResult"
                       style={{
                         background:
-                          lblResult.value === "OK" ? "#52c41a" : "#ff4d4f",
+                          lblResult.value === "OK" ? "#52c41a" : " #BA0900",
                         width: "70%", // ควบคุมขนาดของ Paper
                       }}
                     >
@@ -343,7 +343,7 @@ function ScanSMTRoollSht() {
                     </Paper>
                   </div>
                  
-                  {/* <AntTable 
+                  <AntTable 
                 columns={columns}
                 dataSource={gvScanResult.value}
                 style={{ width:'100%'}}
@@ -351,7 +351,7 @@ function ScanSMTRoollSht() {
                 size="small"
                 bordered
                 className="tableGvResult"
-                /> */}
+                />
                 </>
               )}
             </Grid>
