6b378fb8570161e02a26705377e0dd40156392f8
Mayxsasii <Lovemay19123@gmail.com>
2024-09-24 08:25:35 +0700

    may

--- src/Page/LotTraceView/fc_LotTraceView.jsx
+++ src/Page/LotTraceView/fc_LotTraceView.jsx
@@ -5,22 +5,45 @@ import * as XLSX from "xlsx";
 import { saveAs } from "file-saver";
 import { Tag } from "antd";
 function fc_LotTraceView() {
+  const [txtLotNo, settxtLotNo] = useState("");
+  const [txtSheetNo, settxtSheetNo] = useState("");
+  const [txtSerialNo, settxtSerialNo] = useState("");
+  const [txtProd, settxtProd] = useState("");
+//   txtRollNo url text 
+// 
+  //Focus
+  const fc_txtLotNo = useRef(null);
 
-    const [txtLotNo, settxtLotNo] = useState("");
-    
-    //link
-    const params = new URLSearchParams(window.location.search);
-    const lot = params.get("lot"); 
-    
-    useEffect(() => {
-        if(lot!=''){
-          
-        }  else{
-            // reset()
-        }
-      
-      }, []);
-    return {};
+  //link
+  const params = new URLSearchParams(window.location.search);
+  const lot = params.get("lot");
+
+  useEffect(() => {
+    if (lot == "" || lot == null || lot == undefined) {
+      console.log("lot1", lot);
+      // reset()
+    } else {
+      console.log("lot2", lot);
+      settxtLotNo(lot);
+      // btnSearch_Click(btnSearch, Nothing)
+    }
+    fc_txtLotNo.current.focus();
+  }, []);
+
+  const btnSearch_Click = async () => {
+    let strPrevLot;
+    let strNextLot;
+  };
+
+  const reset = async () => {
+    let strPrevLot;
+    let strNextLot;
+  };
+  return {
+    settxtLotNo,
+    txtLotNo,
+    fc_txtLotNo,
+  };
 }
 
 export { fc_LotTraceView };
