3fb104055504f2b946ae959a597631d4e415de73
khunpan9551 <paiboon.wongthongdee@gmail.com>
2024-10-03 09:18:34 +0700

    commit by khun 03/10/2024

--- src/Page/ScanAVIConfirmResult/fn_ScanAVIConfirmResult.jsx
+++ src/Page/ScanAVIConfirmResult/fn_ScanAVIConfirmResult.jsx
@@ -22,7 +22,7 @@ function fn_ScanAVIConfirmResult() {
     style: {},
   });
   const [hfDefaultColor, setHfDefaultColor] = useState({
-    value: "#FFFFFF",
+    value: "#00CCFF",
     disbled: "",
     visble: false,
     style: {},
