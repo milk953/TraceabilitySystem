175a5a192a24d36a5180dd6d9b33b8de87ade47c
milk953 <milk.the@hotmail.com>
2024-10-15 09:22:55 +0700

    addnewpage

---
+++ src/Page/ScanAOISheetNo/fn_ScanAOISheetNo.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+
+function fn_ScanAOISheetNo() {
+    return {
+
+    }
+};
+
+export { fn_ScanAOISheetNo };
