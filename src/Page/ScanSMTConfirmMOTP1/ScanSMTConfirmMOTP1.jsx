fff655b6370332ba25188465aeb5a7689a7371a1
milk953 <milk.the@hotmail.com>
2024-10-18 08:21:51 +0700

    update

--- src/Page/ScanSMTConfirmMOTP1/ScanSMTConfirmMOTP1.jsx
+++ src/Page/ScanSMTConfirmMOTP1/ScanSMTConfirmMOTP1.jsx
@@ -26,11 +26,19 @@ import { Table as AntTable } from 'antd';
 import "../Common/StyleCommon.css";
 import Pageimg from "/src/assets/1.jpg";
 import BackspaceIcon from "@mui/icons-material/Backspace";
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 import Header from "../Header/Header";
 import "../ScanSMTConfirmMOTP1/ScanSMTConfirmMOTP1.css";
 import { fn_ScanSMTConfirmMOTP1 } from "./fn_ScanSMTConfirmMOTP1";
 
 function ScanSMTConfirmMOTP1() {
+    const {
+        txtLot, settxtLot, selProduct, Productdata, lblLot, pnlLog, lblLog, lblResult, lblResultcolor, pnlSerial,
+        hfSerialCount, gvScanResult, gvScanData, txtgvSerial, inputLot, ddlProduct, inputgvSerial, handleChangeLot,
+        ibtBackClick, handleChangeProduct, handleChangeSerial, handleKeygvSerial, btnSaveClick, btnCancelClick,
+        columnsgvResult, btnHiddenClick
+    } = fn_ScanSMTConfirmMOTP1();
+
     return (
         <div>
             <Header />
@@ -71,31 +79,31 @@ function ScanSMTConfirmMOTP1() {
                                         <TextField
                                             className="input_txt"
                                             size="small"
-                                            // inputRef={(el) => (inputLot.current = el)}
+                                            inputRef={(el) => (inputLot.current = el)}
                                             fullWidth
-                                        // value={txtLot}
-                                        // disabled={txtLotDisabled}
-                                        // style={{
-                                        //     backgroundColor: txtLotDisabled ? "#e0e0e0" : "inherit",
-                                        // }}
-                                        // onChange={(e) => {
-                                        //     settxtLot(e.target.value);
-                                        // }}
-                                        // onKeyDown={(e) => {
-                                        //     if (e.key === "Enter") {
-                                        //         handleChangeLot();
-                                        //     }
-                                        // }}
-                                        // onBlur={() => {
-                                        //     if (txtLot !== "") {
-                                        //         handleChangeLot();
-                                        //     }
-                                        // }}
+                                            value={txtLot.value}
+                                            disabled={txtLot.disabled}
+                                            style={{
+                                                backgroundColor: txtLot.disabled ? "#e0e0e0" : "inherit",
+                                            }}
+                                            onChange={(e) => {
+                                                settxtLot(e.target.value);
+                                            }}
+                                            onKeyDown={(e) => {
+                                                if (e.key === "Enter") {
+                                                    handleChangeLot();
+                                                }
+                                            }}
+                                            onBlur={() => {
+                                                if (txtLot !== "") {
+                                                    handleChangeLot();
+                                                }
+                                            }}
                                         />
                                     </TableCell>
                                     <TableCell>
                                         <Button className="Bt_ibtBack"
-                                        // onClick={ibtBackClick}
+                                            onClick={ibtBackClick}
                                         >
                                             <Tooltip title="Lock" placement="right-end">
                                                 <BackspaceIcon className="Icon_ibtBack" />
@@ -110,16 +118,16 @@ function ScanSMTConfirmMOTP1() {
                                     <TableCell>
                                         <Autocomplete
                                             className="Select_dropDown"
-                                            // disabled={selProDisabled}
-                                            // style={{
-                                            //     backgroundColor: selProDisabled ? "#e0e0e0" : "inherit",
-                                            // }}
-                                            // value={selProduct}
-                                            // onChange={(e, value) => handleChangeProduct(value)}
-                                            // options={Productdata.map((item) => item.prd_name)}
+                                            disabled={selProduct.disabled}
+                                            style={{
+                                                backgroundColor: selProduct.disabled ? "#e0e0e0" : "inherit",
+                                            }}
+                                            value={selProduct.value}
+                                            onChange={(e, value) => handleChangeProduct(value)}
+                                            options={Productdata.map((item) => item.prd_name)}
                                             renderInput={(params) => (
                                                 <TextField
-                                                    // inputRef={(el) => (ddlProduct.current = el)}
+                                                    inputRef={(el) => (ddlProduct.current = el)}
                                                     {...params}
                                                     size="small"
                                                     sx={{ textAlign: "left" }}
@@ -133,140 +141,155 @@ function ScanSMTConfirmMOTP1() {
                                         <Typography>Lot :</Typography>
                                     </TableCell>
                                     <TableCell>
-                                        <Typography>[lblLot]</Typography>
+                                        <Typography>{lblLot}</Typography>
                                     </TableCell>
                                 </TableRow>
                             </TableBody>
                         </Table>
                     </TableContainer>
 
-                    {/* {pnlLog && ( */}
-                    <Paper
-                        elevation={3}
-                        className="Card-lblLog"
-                        style={{
-                            width: "433px",
-                            marginLeft: "23px",
-                        }}
-                    >
-                        {/* {lblLog} */}
-                    </Paper>
-                    {/* )} */}
-
-                    <div className="divgvSerialConfirm" style={{ position: "relative" }}>
-                        <TableContainer
-                            component={Paper}
+                    {pnlLog && (
+                        <Paper
+                            elevation={3}
+                            className="Card-lblLog"
                             style={{
-                                width: "100%",
-                                display: "flex",
-                                flexDirection: "column",
-                                justifyContent: "space-between",
+                                width: "433px",
+                                marginLeft: "23px",
                             }}
                         >
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>No.</TableCell>
-                                        <TableCell>Serial No.</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {/* {Array.from({ length: gvSerialData.length }, (_, index) => ( */}
-                                    <TableRow>
-                                        <TableCell
-                                            align="center"
-                                        >
-                                            {/* {index + 1} */}
-                                        </TableCell>
-                                        <TableCell>
-                                            <TextField
-                                                className="input_txt"
-                                                size="small"
-                                                fullWidth
-                                            // value={txtgvSerial[index] || ""}
-                                            // inputRef={el => inputgvSerial.current[index] = el}
-                                            // onChange={(e) => {
-                                            //     handleChangeSerial(index, e);
-                                            // }}
-                                            // onKeyDown={(e) => handleKeygvSerial(e, index)}
-                                            // onBlur={(e) => handleKeygvSerial(e, index)}
-                                            />
-                                        </TableCell>
-                                    </TableRow>
-                                    {/* ))} */}
-                                </TableBody>
-                            </Table>
-                            <div style={{
-                                marginTop: "auto",
-                                display: "flex",
-                                justifyContent: "center",
-                                gap: "10px",
-                                marginLeft: "5px",
-                                marginBottom: "2px"
-                            }}
+                            {lblLog}
+                        </Paper>
+                    )}
+
+                    {pnlSerial && (
+                        <div className="divgvSerialConfirm" style={{ position: "relative" }}>
+                            <TableContainer
+                                component={Paper}
+                                style={{
+                                    width: "100%",
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    justifyContent: "space-between",
+                                }}
                             >
-                                <Button
-                                    className="BtSave"
-                                // onClick={btnSaveClick}
+                                <Table>
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell>No.</TableCell>
+                                            <TableCell>Serial No.</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {Array.from({ length: hfSerialCount.length }, (_, index) => (
+                                            <TableRow>
+                                                <TableCell
+                                                    align="center"
+                                                >
+                                                    {index + 1}
+                                                </TableCell>
+                                                <TableCell>
+                                                    <TextField
+                                                        className="input_txt"
+                                                        size="small"
+                                                        fullWidth
+                                                        value={txtgvSerial[index] || ""}
+                                                        inputRef={el => inputgvSerial.current[index] = el}
+                                                        onChange={(e) => {
+                                                            handleChangeSerial(index, e);
+                                                        }}
+                                                        onKeyDown={(e) => handleKeygvSerial(e, index)}
+                                                        onBlur={(e) => handleKeygvSerial(e, index)}
+                                                    />
+                                                </TableCell>
+                                            </TableRow>
+                                        ))}
+                                    </TableBody>
+                                </Table>
+                                <div style={{
+                                    marginTop: "auto",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    gap: "10px",
+                                    marginLeft: "5px",
+                                    marginBottom: "2px"
+                                }}
                                 >
-                                    Save
-                                </Button>{" "}
-                                &nbsp;&nbsp;
-                                <Button
-                                    className="BtCancel"
-                                // onClick={btnCancelClick}
-                                >
-                                    Cancel
-                                </Button>
-                            </div>
-                        </TableContainer>
-                    </div>
+                                    <Button
+                                        className="BtSave"
+                                        onClick={btnSaveClick}
+                                    >
+                                        Save
+                                    </Button>{" "}
+                                    &nbsp;&nbsp;
+                                    <Button
+                                        className="BtCancel"
+                                        onClick={btnCancelClick}
+                                    >
+                                        Cancel
+                                    </Button>
+                                </div>
+                            </TableContainer>
+                        </div>
+                    )}
+                    <Button
+                        style={{
+                            width: '1px',
+                            height: '1px',
+                            padding: "0px"
+                        }}
+                        onClick={btnHiddenClick}
+                    >
+                        <FileDownloadOutlinedIcon />
+                    </Button>
                 </Box>
 
                 <div className="divgvScanResultCon">
-                    <>
-                        {/* <img
-                            className="Img_GvResult"
-                            style={{ marginLeft: "35%", }}
-                            src={Pageimg} // Import the image
-                            alt="Description of the image"
-                        /> */}
-                    </>
-                    <>
 
-                        <div style={{ display: "flex", gap: "10px", width: "100%" }}>
-                            <Paper
-                                className="Card-lblResult"
-                                style={{
-                                    // background: lblResultcolor,
-                                    width: "100%",
-                                    marginLeft: "25px"
-                                }}
-                            >
-                                <Typography
-                                    variant="h4"
-                                    style={{ paddingTop: "5px", color: "#fff" }}
+                    {gvScanResult === false && (
+                        <>
+                            <img
+                                className="Img_GvResult"
+                                style={{ marginLeft: "35%", }}
+                                src={Pageimg} // Import the image
+                                alt="Description of the image"
+                            />
+                        </>
+                    )}
+
+                    {gvScanResult && (
+                        <>
+                            <div style={{ display: "flex", gap: "10px", width: "100%" }}>
+                                <Paper
+                                    className="Card-lblResult"
+                                    style={{
+                                        background: lblResultcolor,
+                                        width: "100%",
+                                    }}
                                 >
-                                    {/* {lblResult} */}
-                                </Typography>
-                            </Paper>
-                        </div>
-                        <br />
-                        <AntTable
-                            // columns={columns}
-                            // dataSource={gvScanData}
-                            // rowKey={(record) => record.SEQ}
-                            style={{
-                                width: '98%',
-                                marginLeft: "22px"
-                            }}
-                            pagination={false}
-                            size="small"
-                            bordered
-                            className="tableGvResult"
-                        />
+                                    <Typography
+                                        variant="h4"
+                                        style={{ paddingTop: "5px", color: "#fff" }}
+                                    >
+                                        {lblResult}
+                                    </Typography>
+                                </Paper>
+                            </div>
+                            <br />
+                            <AntTable
+                                columns={columnsgvResult}
+                                dataSource={gvScanData}
+                                rowKey={(record) => record.SEQ}
+                                style={{
+                                    width: '100%',
+                                }}
+                                pagination={false}
+                                size="small"
+                                bordered
+                                className="tableGvResult"
+                            />
 
-                    </>
+                        </>
+                    )}
                 </div>
             </Card>
         </div>
