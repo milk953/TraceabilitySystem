002f55f5601400a67cad79bd790c1dbab3a42e16
chayanon12 <chayanonolo@gmail.com>
2025-06-27 09:31:46 +0700

    pond delete css

--- src/Page/ScanSheetReflowTime/ScanSheetReflowTime.jsx
+++ src/Page/ScanSheetReflowTime/ScanSheetReflowTime.jsx
@@ -8,9 +8,9 @@ import {
   TableHead,
   Paper,
   Card,
-  Button as MuiBtn
+  Button as MuiBtn,
 } from "@mui/material";
-import { Button } from 'antd';
+import { Button } from "antd";
 import "./ScanSheetReflowTime.css";
 import "../Common/StyleCommon.css";
 import BackspaceIcon from "@mui/icons-material/Backspace";
@@ -40,26 +40,52 @@ function ScanSheetReflowTime() {
     btnReplace_Click,
     btnDelete_Click,
     btnIbtback_Click,
-    PnlShowresult
+    PnlShowresult,
   } = fn_ScanSheetReflowTime();
+  function setFocus(id) {
+    document.getElementById(id).focus();
+  }
+ useEffect(() => {
+  const handleExternalSet = (e) => {
+    const id = e.type; 
+    const value = e.detail;
+    switch (id) {
+      case "txtMCNo":
+        setTxtmcNo(value);
+        break;
+      case "txtSheetNo":
+        setTxtSheetNo(value);
+        break;
+    }
+  };
+
+  const fields = ["txtMCNo", "txtSheetNo"]; 
+
+  fields.forEach((field) => window.addEventListener(field, handleExternalSet));
+  return () => {
+    fields.forEach((field) => window.removeEventListener(field, handleExternalSet));
+  };
+}, []);
+
+
   useEffect(() => {
     if (txtSheetNoState.state == true && txtSheetNo == "") {
-      FctxtSheetNo.current.focus();
+      setFocus("txtSheetNo");
     }
     if (txtmcNo == "") {
-      FctxtmcNo.current.focus();
+      setFocus("txtMCNo");
     }
   }, [txtSheetNoState, txtSheetNo, txtmcNo]);
   const { menuName } = fn_Homepage();
   return (
     <div>
       <Hearder />
-      <div style={{marginTop:'80px'}}></div>
+      <div style={{ marginTop: "80px" }}></div>
       <div className="pnlMain">
         <Table id="TableMain" component={Card}>
           <TableHead>
             <TableRow>
-              <TableCell colSpan={3} >{menuName}</TableCell>
+              <TableCell colSpan={3}>{menuName}</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -76,7 +102,6 @@ function ScanSheetReflowTime() {
                   onChange={(e) => {
                     setTxtmcNo(e.target.value);
                   }}
-                  // onBlur={handleTxtMcNo}
                   onKeyDown={(e) => {
                     if (e.key === "Enter") {
                       handleTxtMcNo();
@@ -86,7 +111,7 @@ function ScanSheetReflowTime() {
                 ></TextField>
               </TableCell>
               <TableCell>
-                <MuiBtn  id='ibtback' onClick={btnIbtback_Click}>
+                <MuiBtn id="ibtback" onClick={btnIbtback_Click}>
                   <BackspaceIcon />
                 </MuiBtn>
               </TableCell>
@@ -96,7 +121,7 @@ function ScanSheetReflowTime() {
               <TableCell>
                 <TextField
                   size="small"
-                  id='txtSheetNo'
+                  id="txtSheetNo"
                   className="txtFieldReflow"
                   disabled={txtSheetNoState.disabled}
                   sx={txtSheetNoState.styled}
@@ -116,7 +141,11 @@ function ScanSheetReflowTime() {
               <TableCell></TableCell>
             </TableRow>
             <TableRow>
-              <TableCell colSpan={3} sx={{ textAlign: "center",fontSize:'14px' }} id='lblSheet'>
+              <TableCell
+                colSpan={3}
+                sx={{ textAlign: "center", fontSize: "14px" }}
+                id="lblSheet"
+              >
                 {lblSheet}
               </TableCell>
             </TableRow>
@@ -124,52 +153,85 @@ function ScanSheetReflowTime() {
         </Table>
       </div>
       {PnlShowresult && (
-      <div className="pnlResult">
-        <Table  id={lblResult.text == "NG" ? "TableResultReflowred" : "TableResultReflow"} component={Card}>
-          <TableRow>
-            <TableCell
-              sx={{
-                fontSize: "60px",
-                padding: "0px",
-                color: lblResult.styled?.color || "defaultColor",
-                background: lblResult.styled?.background || "defaultBackground",
-              }}
-              id="lblResult"
-            >
-              {lblResult.text}
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell
-              sx={{
-                fontSize: "34px",
-                padding: "0px",
-                
-                color: lblRemark.styled?.color || "defaultColor",
-                background: lblRemark.styled?.background || "defaultBackground",
-                border : lblRemark.styled?.border || "defaultBorder",
-                whiteSpace: "pre-line"
-              }}
-              id="lblRemark"
-            >
-              {lblRemark.text}
-            </TableCell>
-          </TableRow>
-        </Table>
-      </div>
+        <div className="pnlResult">
+          <Table
+            id={
+              lblResult.text == "NG"
+                ? "TableResultReflowred"
+                : "TableResultReflow"
+            }
+            component={Card}
+          >
+            <TableRow>
+              <TableCell
+                sx={{
+                  fontSize: "60px",
+                  padding: "0px",
+                  color: lblResult.styled?.color || "defaultColor",
+                  background:
+                    lblResult.styled?.background || "defaultBackground",
+                }}
+                id="lblResult"
+              >
+                {lblResult.text}
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell
+                sx={{
+                  fontSize: "34px",
+                  padding: "0px",
+
+                  color: lblRemark.styled?.color || "defaultColor",
+                  background:
+                    lblRemark.styled?.background || "defaultBackground",
+                  border: lblRemark.styled?.border || "defaultBorder",
+                  whiteSpace: "pre-line",
+                }}
+                id="lblRemark"
+              >
+                {lblRemark.text}
+              </TableCell>
+            </TableRow>
+          </Table>
+        </div>
       )}
       {pnlSaveState && (
         <div className="pnlSave">
           <Table id="TableSave" component={Card}>
             <TableRow>
               <TableCell>
-                <Button variant="contained" id='btnReplace' className="ButtonReplace" size="middle" onClick={btnReplace_Click}>Replace</Button>
+                <Button
+                  variant="contained"
+                  id="btnReplace"
+                  className="ButtonReplace"
+                  size="middle"
+                  onClick={btnReplace_Click}
+                >
+                  Replace
+                </Button>
               </TableCell>
               <TableCell>
-                <Button variant="contained" id='btnDelete' className="ButtonDelete" size="middle" onClick={btnDelete_Click}>Delete</Button>
+                <Button
+                  variant="contained"
+                  id="btnDelete"
+                  className="ButtonDelete"
+                  size="middle"
+                  onClick={btnDelete_Click}
+                >
+                  Delete
+                </Button>
               </TableCell>
               <TableCell>
-                <Button variant="contained" id='btnCancel' className="ButtonCancel" size="middle" onClick={btnCancel_Click}>Cancel</Button>
+                <Button
+                  variant="contained"
+                  id="btnCancel"
+                  className="ButtonCancel"
+                  size="middle"
+                  onClick={btnCancel_Click}
+                >
+                  Cancel
+                </Button>
               </TableCell>
             </TableRow>
           </Table>
